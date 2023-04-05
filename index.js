eced113dfeb5605db9a0f38f7ffc1ff3014bdf37
Eliana Echeverria <eliana.giselle82@yahoo.com>
2023-04-04 17:31:52 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  
+  let result
+  if(feet <= 400) {
+    result = "This one is on me!";
+  }
+  if (feet > 400 && feet <= 2000){
+    result = "That will be twenty bucks.";
+  }
+  if (feet > 2000 && feet <= 2500){
+    result = "I will gladly take your thirty bucks."
+  }
+  else if (feet > 2500){
+    return "No can do."
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+function ternaryCheckCity(city){
+  if (city=== 'NYC')
+  return 'Ok, sounds good.';
+
+  else (city !== 'Pittsburg')
+  return 'No go.';
+  }
+
+function switchOnCharmFromTip(tip){
+  
+  switch (tip) {
+    case "generous":
+      return("Thank you so much.");
+      case "not as generous":
+      return ("Thank you.");
+      default: "if anything else";
+        return ("Bye.");
+      }}
